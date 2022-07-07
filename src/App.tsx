5b6125a5ba4860ec36a397da6037ae26cee59f87
bekadev <bekapng@gmail.com>
2022-07-07 22:36:09 +0600

    commit - props - propstype - map

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Raiting} from "./components/raiting/Raiting";
+import {Accordion} from "./components/accordion/Accordion";
+import {Map} from "./components/map/Map";
 
 function App() {
+
+    const students = [
+        {id: 1, name: "James", age: 8},
+        {id: 2, name: "Robert", age: 18},
+        {id: 3, name: "John", age: 28},
+        {id: 4, name: "Michael", age: 38},
+        {id: 5, name: "William", age: 48},
+        {id: 6, name: "David", age: 58},
+        {id: 7, name: "Richard", age: 68},
+        {id: 8, name: "Joseph", age: 78},
+        {id: 9, name: "Thomas", age: 88},
+        {id: 10, name: "Charles", age: 98},
+        {id: 11, name: "Christopher", age: 100},
+    ]
+
+    const nameBase = [
+        {id: 1, name: "Beka", age: 21},
+        {id: 2, name: "Ayan", age: 20},
+        {id: 3, name: "Kuat", age: 22}
+    ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div>
+            <Raiting value={3}/>
+            <Accordion title={'night'}/>
+            <Map students={students} nameBase={nameBase}/>
+        </div>
     </div>
   );
 }
