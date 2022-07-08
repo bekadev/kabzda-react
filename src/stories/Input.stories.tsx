3e0f3fc5a206bb5e1f13d6256eed6f8ad1e04fc7
bekadev <bekapng@gmail.com>
2022-07-08 23:28:53 +0600

    commit - input checkbox selected controlled

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {action} from "@storybook/addon-actions";
 
 
 
@@ -16,4 +17,37 @@ export const UnControlledInput = () => {
         }
     }/> - {value} </>
 }
-export const ControlledInput = () => <input value={'kz'}/>
+
+
+export const ControlledCheckBox = () => {
+    const [parentValue, setParantValue] = useState(true)
+
+    const onChangeHandlerInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setParantValue(e.currentTarget.checked)
+    }
+    return <input type='checkbox' checked={parentValue} onChange={onChangeHandlerInput}/>
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParantValue] = useState('')
+
+    const onChangeHandlerInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setParantValue(e.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChangeHandlerInput}/>
+}
+
+export const ControlledSelected = () => {
+    const [parentValue, setParantValue] = useState<string | undefined>('2')
+
+    const onChangeHandlerInput = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParantValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeHandlerInput}>
+        <option>none</option>
+        <option value={1}>minsk</option>
+        <option value={2}>astana</option>
+        <option value={3}>almaty</option>
+    </select>
+}
+
